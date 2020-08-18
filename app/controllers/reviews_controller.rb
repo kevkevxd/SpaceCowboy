87224bc50e6638bd7eb6e5c44d02c8c410fccaf5
kevin_xie <xie.kevin.j@gmail.com>
2020-08-18 18:06:20 -0400

    3rd commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,31 @@
 class ReviewsController < ApplicationController
+    before_action :find_reviews, only: [:edit, :update, :destroy]
+    def new
+        @review = Review.new
+    end
+    def create 
+        review = Review.new(review_params)   
+        if @review.save 
+            redirect_to @review
+        else
+            render 'new'
+        end
+    end
+    def edit;end
+    def update
+        @review.update(review_params)
+        redirect_to review_path(@review)
+    end
+
+    def destroy
+        # find_reviews
+    end
+private
+    def find_reviews
+        @review = Review.find(params[:id])
+    end
+
+    def review_params
+        params.require(:review).permit(:user_id, :anime_id, :rating)
+    end 
 end
--- app/controllers/trackeds_controller.rb
+++ app/controllers/trackeds_controller.rb
@@ -1,5 +1,35 @@
 class TrackedsController < ApplicationController
-    def index
+    before_action :find_tracked, only: [:edit, :update, :destroy]
+   
+    def new
+        @tracked = Tracked.new
+    end
+
+    def edit;end
+
+    def create 
+        tracked = Tracked.create(tracked_params)   
+            redirect_to @tracked 
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @tracked.update(tracked_params)
+        redirect_to tracked_path(@tracked)
+    end
+
+    def destroy
+        # find_tracked
+    end
+private
+    def find_tracked
+        @tracked = Tracked.find(params[:id])
+    end
+
+    def tracked_params
+        params.require(:tracked).permit(:user_id, :anime_id, :rating)
     end
 
 end
