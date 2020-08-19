e435ce168fdcfce6650c79395f27250477a90ed3
kevin_xie <xie.kevin.j@gmail.com>
2020-08-19 17:00:20 -0400

    more pushes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,38 @@
 class SessionsController < ApplicationController
 
 
+
   def logout
     session.delete(:user_id)
 
     redirect_to animes_path
   end
 
+  def new
+  
+  end
 
-
+  def create
+  user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash[:errors] = user.errors.full_messages
+      redirect_to new_user_path
+    end
+  end
 
 end
+
+
+# def create 
+#   user = User.find_by(name: params[:session][:name])
+
+#   if user && user.authenticate(params[:session][:password])
+#     session[:user_id] = user.id 
+#     redirect_to user 
+#   else 
+#     flash[:error] = "username or password is incorrect"
+#     redirect_to new_login_path
+#   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@
   resources :trackeds
   resources :reviews
   
-de
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
