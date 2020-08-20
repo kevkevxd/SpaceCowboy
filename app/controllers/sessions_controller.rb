5b6731607f099c186ae0a4804124aaf8c73acad3
kevin_xie <xie.kevin.j@gmail.com>
2020-08-20 00:35:21 -0400

    MVP FIN

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,50 +1,25 @@
 class SessionsController < ApplicationController
+# skip_before_action :authorized, only: [:new, :create]
 
-  def logout
-    session.delete(:user_id)
-
-    redirect_to animes_path
-  end
-
+  
   def new
   end
 
-  def destroy
+  def logout
+    session.delete(:user_id)
+    redirect_to new_login_path
   end
-  end
+  
 
   def create
-    render 'new'
-
     @user = User.find_by(name: params[:session][:name])
-    if @user&& user.authenticate(params[:session][:password])
+    if @user&& @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to user
+      redirect_to user_path(@user)
     else
       flash[:errors] = "Username or Password is Incorrect"
       redirect_to new_login_path
     end
   end
-
-  def logout
-    session.delete(:user_id)
-
-    redirect_to new_user_path
-  end
-
-
-
 end
 
-
-# def create 
-#   user = User.find_by(name: params[:session][:name])
-
-#   if user && user.authenticate(params[:session][:password])
-#     session[:user_id] = user.id 
-#     redirect_to user 
-#   else 
-#     flash[:error] = "username or password is incorrect"
-#     redirect_to new_user_path
-#   end
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
  Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'sessions/new', to: 'sessions#new', as: "new_login"
+  post 'sessions/create', to: 'sessions#create', as: 'login'
+  delete 'sessions/logout', to: 'sessions#logout', as: 'logout'
+
   resources :users
   resources :animes
   resources :trackeds
   resources :reviews
-  
-
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
